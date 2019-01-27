683da128bbb105a198214f602db418fd58329ee2
vivek <akellavivek1999@gmail.com>
2019-01-27 15:26:24 +0530

    Profile Activity added with bottom navigation fragments

---
+++ app/src/main/java/com/vivek/hisab/Profile2Activity.java
@@ -0,0 +1,120 @@
+package com.vivek.hisab;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Profile2Activity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, notifications.OnFragmentInteractionListener, Home.OnFragmentInteractionListener, friends.OnFragmentInteractionListener {
+
+    private FirebaseAuth mAuth;
+    private TextView mTextMessage;
+    private friends fri;
+    private Home home;
+    private notifications not;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile2);
+        FirebaseApp.initializeApp(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loadFragment(new Home());
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent startIntent = new Intent(Profile2Activity.this, MainActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new Home();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new friends();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new notifications();
+                break;
+
+
+        }
+
+        return loadFragment(fragment);
+    }
+
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/vivek/hisab/ProfileActivity.java
+++ app/src/main/java/com/vivek/hisab/ProfileActivity.java
@@ -1,14 +1,43 @@
 package com.vivek.hisab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent startIntent = new Intent(ProfileActivity.this, MainActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
